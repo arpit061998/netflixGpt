ee0bc9f144e9f9d8069fb0acf99aa8700a3072a0
kPoint <dev@kpoint.com>
2023-09-07 22:07:07 +0530

    checking all config file ~arpit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <h1 className='text-3xl text-green-100 font-bold underline'>This is Arpit Netflix Gpt App</h1>
+    </>
+  )
+}
+export default App;
